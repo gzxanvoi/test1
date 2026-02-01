27e37f31cd1e757d7fc80d54b434aaa215819c7d
gzxanvoi <gzxanvoi@gmail.com>
2026-02-01 18:06:30 +0800

    feat:初步创建demo

---
+++ src/bad_code.py
@@ -0,0 +1,16 @@
+def HelloWorld():
+    x = 1
+    y = 2
+    z = x + y
+    print("Sum is", z)
+    a = "hello"
+    b = "world"
+    c = a + b
+    print(c)
+    if x == 1:
+        print("x is one")
+    for i in range(3):
+        print(i)
+
+
+HelloWorld()
